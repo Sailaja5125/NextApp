f24a8422eadfdd254bf76da5191d950993037391
Sailaja5125 <sailajapuvala5125@gmail.com>
2024-10-11 21:43:45 +0530

    "djskjfhdkghisoo"

--- appwrite/src/app/api/users/logout/route.ts
+++ appwrite/src/app/api/users/logout/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(): Promise<NextResponse<{ message: string; success: boolean; }> | NextResponse<{ error: any; }>>{
   try {
-    const response = await NextResponse.json({
+    const response = NextResponse.json({
         message:"Logout Successful",
         success:true
     })
